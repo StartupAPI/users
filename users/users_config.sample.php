afad39f775b6f6eeb0cedadbdecb3e584b0bbde7
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2010-04-17 19:49:06 +0000

    Added subfolder includes as users_config.php should live in the folder above to be properly versioned in parent project.

--- users/users_config.sample.php
+++ users/users_config.sample.php
@@ -1,19 +1,17 @@
 <?
-require_once('default_config.php');
+#require_once('users/modules/facebook/index.php');
+#UserConfig::$modules[] = new FacebookAuthenticationModule('...................', '......................');
 
-require_once('modules/facebook/index.php');
-UserConfig::$modules[] = new FacebookAuthenticationModule('...................', '......................');
+#require_once('users/modules/google/index.php');
+#UserConfig::$modules[] = new GoogleAuthenticationModule('....................');
 
-require_once('modules/google/index.php');
-UserConfig::$modules[] = new GoogleAuthenticationModule('....................');
-
-require_once('modules/usernamepass/index.php');
+require_once('users/modules/usernamepass/index.php');
 UserConfig::$modules[] = new UsernamePasswordAuthenticationModule();
 
 UserConfig::$SESSION_SECRET= '..................................................';
 
 UserConfig::setDB(new mysqli( 'localhost', 'user1', 'mypass', 'mydb'));
 
-UserConfig::$header = dirname(dirname(__FILE__)).'/header.php';
-UserConfig::$footer = dirname(dirname(__FILE__)).'/footer.php';
+UserConfig::$header = dirname(__FILE__).'/header.php';
+UserConfig::$footer = dirname(__FILE__).'/footer.php';
 
