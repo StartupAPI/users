57f948a175f925bb1b7b7489e51ed8b6c2b700f7
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2011-02-01 15:38:46 -0400

    Error page for unauthenticated users.

---
+++ admin/admin_access_only.php
@@ -0,0 +1,9 @@
+<?php
+require_once(UserConfig::$header);
+
+?><h2>Acced Denied</h2>
+
+<p>This page can only be accessed by administrators of this site</p>
+
+<?php
+require_once(UserConfig::$footer);
