da0a4e0e1d6ed89d5d14f00e1b458822879e13ff
whale2 <whale2.box@gmail.com>
2012-02-19 03:02:01 +0400

    Fix in smarty v2 support

--- account_details.php
+++ account_details.php
@@ -9,9 +9,9 @@ if (preg_match("/^Smarty-3/",$smarty->_version)) {
   $smarty->setCompileDir(UserConfig::$smarty_compile);
   $smarty->setCacheDir(UserConfig::$smarty_cache);
 } elseif (preg_match("/^2\./",$smarty->_version)) {
-  $smarty->template_dir(UserConfig::$smarty_templates.'/account');
-  $smarty->compile_dir(UserConfig::$smarty_compile);
-  $smarty->cache_dir(UserConfig::$smarty_cache);
+  $smarty->template_dir = UserConfig::$smarty_templates.'/account';
+  $smarty->compile_dir = UserConfig::$smarty_compile;
+  $smarty->cache_dir = UserConfig::$smarty_cache;
 } else {
   die("Cannot handle smarty version ".$smarty->_version);
 }
