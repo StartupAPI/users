ac5d9de94a27a7eb7cb2bccce354c3cc24ee2e2c
whale2 <whale2.box@gmail.com>
2012-02-19 02:56:55 +0400

    smarty v2 support, ggg

--- account_details.php
+++ account_details.php
@@ -4,9 +4,17 @@ require_once(dirname(__FILE__).'/User.php');
 
 include(dirname(__FILE__).'/view/account/account_details.php');
 # this yields Smarty onbject as $smarty 
-$smarty->setTemplateDir(UserConfig::$smarty_templates.'/account');
-$smarty->setCompileDir(UserConfig::$smarty_compile);
-$smarty->setCacheDir(UserConfig::$smarty_cache);
+if (strstr($smarty->_version,"Smarty-3")) {
+  $smarty->setTemplateDir(UserConfig::$smarty_templates.'/account');
+  $smarty->setCompileDir(UserConfig::$smarty_compile);
+  $smarty->setCacheDir(UserConfig::$smarty_cache);
+} elseif (strstr($smarty->_version,"Smarty-2")) {
+  $smarty->template_dir(UserConfig::$smarty_templates.'/account');
+  $smarty->compile_dir(UserConfig::$smarty_compile);
+  $smarty->cache_dir(UserConfig::$smarty_cache);
+} else {
+  die "Cannot handle smarty version ".$smarty->_version;
+}
 
 require_once(UserConfig::$header);
 
