e3ead775d9b5e5ceaf6794907e1cb3600842f9cb
Paul Sutyrin <paul.sutyrin@gmail.com>
2012-02-23 01:23:32 +0400

    pluggable current time

--- CurrentTime.php
+++ CurrentTime.php
@@ -1,13 +1,26 @@
 <?php
 
-class RealCurrentTime  {
+class CurrentTime {
+
+  function format( $format )
+  {
+    return $this -> getDateTime() -> format ( $format );
+  }
+
+  function date ( $format )
+  {
+    return $this -> format ( $format );
+  }
+}
+
+class RealCurrentTime extends CurrentTime {
   function getDateTime()
   {
     return new DateTime();
   }
 }
 
-class ModelledCurrentTime {
+class ModelledCurrentTime extends CurrentTime {
   private $datetime;
 
   function __construct() {
@@ -19,19 +32,9 @@ class ModelledCurrentTime {
     return $this -> datetime;
   }
 
-  function addTime( $interval )
+  function addTime( $date_interval ) 
   {
-    $this -> datetime -> add( $interval );
-  }
-
-  function format( $format )
-  {
-    return $this -> datetime -> format ( $format );
-  }
-
-  function date ( $format )
-  {
-    return $this -> format ( $format );
+    $this -> datetime -> add( new DateInterval ( $date_interval ));
   }
 
 }
