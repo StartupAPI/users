9d0720a393b4dd3a7c1e2a036238ae6c4c05ef02
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-02-25 01:06:46 -0400

    Fixed Smarty setup on plans.php

--- plans.php
+++ plans.php
@@ -3,18 +3,11 @@ require_once(dirname(__FILE__).'/config.php');
 require_once(dirname(__FILE__).'/User.php');
 
 include(dirname(__FILE__).'/view/account/plans.php');
+
 # this yields Smarty object as $smarty 
-if (preg_match("/^Smarty-3/",$smarty->_version)) {
-  $smarty->setTemplateDir(UserConfig::$smarty_templates.'/account');
-  $smarty->setCompileDir(UserConfig::$smarty_compile);
-  $smarty->setCacheDir(UserConfig::$smarty_cache);
-} elseif (preg_match("/^2\./",$smarty->_version)) {
-  $smarty->template_dir = UserConfig::$smarty_templates.'/account';
-  $smarty->compile_dir = UserConfig::$smarty_compile;
-  $smarty->cache_dir = UserConfig::$smarty_cache;
-} else {
-  die("Cannot handle smarty version ".$smarty->_version);
-}
+$smarty->setTemplateDir(UserConfig::$smarty_templates.'/account');
+$smarty->setCompileDir(UserConfig::$smarty_compile);
+$smarty->setCacheDir(UserConfig::$smarty_cache);
 
 require_once(UserConfig::$header);
 
