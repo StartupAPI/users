a4caec5ccadf2a074dc605992817e8889fefd2f5
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-03-17 00:22:42 -0400

    Moving plan structures initialization outside of static config initalization methods to after user configuration changes are made.

--- config.php
+++ config.php
@@ -5,3 +5,8 @@ header('Content-type: text/html; charset=utf-8');
 
 require_once(dirname(__FILE__).'/default_config.php');
 require_once(dirname(dirname(__FILE__)).'/users_config.php');
+
+// Initializing more structures based on user configurations
+if(UserConfig::$useAccounts && UserConfig::$useSubscriptions) {
+	Plan::init(UserConfig::$PLANS);
+}
