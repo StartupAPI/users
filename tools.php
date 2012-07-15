efe47ca25b1343945b2969fff1359c397aa8dbf1
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-07-15 03:01:14 -0400

    Keeping a few nonces, not just the latest one to compare to to limit the failure rate.

--- tools.php
+++ tools.php
@@ -26,24 +26,46 @@ class UserTools
 		/*
 		 * Preventing CSRFs in all POST requests by double-posting cookies
 		 */
+		$passed_nonces = explode(' ', $storage->fetch(UserConfig::$csrf_nonce_key));
+		UserTools::debug('Current nonces: '.implode(' ', $passed_nonces));
+
+		// rolling array to keep a few nonces
+		$unused_nonces = array();
+
 		if (count($_POST) > 0) {
 			if (!array_key_exists('CSRF_NONCE', $_POST)) {
-				error_log('POST request in admin interface without CSRF nonce. Make sure form includes CSRF_NONCE hidden field.');
+				UserTools::debug('POST request in admin interface without CSRF nonce. Make sure form includes CSRF_NONCE hidden field.');
 				header('HTTP/1.0 403 POST request origin check failed', true, 403);
 				exit;
 			}
 
-			$passed_nonce = $storage->fetch(UserConfig::$csrf_nonce_key);
+			$nonce_matched = false;
+			foreach ($passed_nonces as $passed_nonce) {
+				if ($passed_nonce == $_POST['CSRF_NONCE']) {
+					UserTools::debug('Nonce matched: '.$passed_nonce);
+					$nonce_matched = true;
+				} else {
+					$unused_nonces[] = $passed_nonce;
+				}
+			}
 
-			if ($passed_nonce != $_POST['CSRF_NONCE']) {
-				error_log('[Possible CSRF attach] POST request with wrong nonce!!!');
+			if (!$nonce_matched) {
+				UserTools::debug('[Possible CSRF attack] POST request with wrong nonce!!!');
 				header('HTTP/1.0 403 POST request origin check failed', true, 403);
 				exit;
 			}
+		} else {
+			$unused_nonces = $passed_nonces;
 		}
 
 		self::$CSRF_NONCE = base64_encode(mcrypt_create_iv(50, MCRYPT_DEV_URANDOM));
-		$storage->store(UserConfig::$csrf_nonce_key, self::$CSRF_NONCE);
+
+		// adding new nonce in front
+		array_unshift($unused_nonces, self::$CSRF_NONCE);
+		// keeping at most 3 nounces
+		$unused_nonces = array_splice($unused_nonces, 0, 3);
+
+		$storage->store(UserConfig::$csrf_nonce_key, implode(' ', $unused_nonces));
 	}
 
 	public static function renderCSRFNonce() {
