b144ecbdc6290da654234b727b8fe925f34955b3
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-10-03 09:15:03 -0400

    Removed boilerplate code copied from UserNamepass Added documentation

--- modules/email/index.php
+++ modules/email/index.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * @package StartupAPI
- * @subpackage Authentication\Email
- *
- * Email authentication module
+ * Email authentication module (UNFINISHED)
  *
  * This module is used for authenticating users using just an email address.
- * IT is useful for signing up people for future newsletter notifications of for an early beta program.
+ * It is useful for signing up people for future newsletter notifications of for an early beta program.
+ *
+ * @todo Finish the implementation ;)
+ *
+ * @package StartupAPI
+ * @subpackage Authentication\Email
  */
 class EmailAuthenticationModule extends AuthenticationModule
 {
@@ -25,6 +27,15 @@ class EmailAuthenticationModule extends AuthenticationModule
 		return "Email";
 	}
 
+	/**
+	 * Returns user's credentials (email address)
+	 *
+	 * @param User $user User object
+	 *
+	 * @return EmailUserCredentials|null
+	 *
+	 * @throws DBException
+	 */
 	public function getUserCredentials($user)
 	{
 		$db = UserConfig::getDB();
@@ -62,6 +73,13 @@ class EmailAuthenticationModule extends AuthenticationModule
 		return null;
 	}
 
+	/**
+	 * Returns a number of users with who provided email address
+	 *
+	 * @return int Number of users with email address
+	 *
+	 * @throws DBException
+	 */
 	public function getTotalConnectedUsers()
 	{
 		$db = UserConfig::getDB();
@@ -103,7 +121,7 @@ class EmailAuthenticationModule extends AuthenticationModule
 		</fieldset>
 		</form>
 		<?php
-		
+
 	}
 
 	public function renderRegistrationForm($full = false, $action = null, $errors = null, $data = null)
@@ -148,9 +166,20 @@ class EmailAuthenticationModule extends AuthenticationModule
 		<?php
 	}
 
+	/**
+	 * Instead of logging user in, sends a link to their email address for clicking back
+	 *
+	 * @param array $data Form array
+	 * @param boolesn $remember Remember user or not
+	 *
+	 * @return null Never returns a user object
+	 *
+	 * @todo implement actual email sending
+	 */
 	public function processLogin($data, &$remember)
 	{
 		$user = User::getUsersByEmailOrUsername($data['email']);
+//		$user->sendConfirmationEmail();
 
 		header('Location: '.UserConfig::$USERSROOTURL.'/modules/email/login.php?module=email&message=linksent');
 		exit;
@@ -158,6 +187,18 @@ class EmailAuthenticationModule extends AuthenticationModule
 		return null; // kind-of pointless after redirect, but indicates that you can' just login using email
 	}
 
+	/**
+	 * Processes login link and logs user in
+	 *
+	 * @param string $email email address
+	 * @param type $code
+	 *
+	 * @return User User object if successfully logged in
+	 *
+	 * @throws InputValidationException
+	 *
+	 * @todo Finish implementation
+	 */
 	public function processLoginLink($email, $code)
 	{
 		$errors = array();
@@ -183,6 +224,17 @@ class EmailAuthenticationModule extends AuthenticationModule
 		return null;
 	}
 
+	/**
+	 * REgisters a user, but instead of logging them in, sends a link to their email address for clicking back
+	 *
+	 * @param array $data Form data
+	 * @param boolean $remember Remember user or not
+	 *
+	 * @return null Never returns a user object
+	 *
+	 * @throws InputValidationException
+	 * @throws ExistingUserException
+	 */
 	public function processRegistration($data, &$remember)
 	{
 		$errors = array();
@@ -231,113 +283,28 @@ class EmailAuthenticationModule extends AuthenticationModule
 		$user = User::createNewWithoutCredentials($this, $name, $email);
 		$user->recordActivity(USERBASE_ACTIVITY_REGISTER_EMAIL);
 //		$user->sendConfirmationEmail();
-		return $user;
+		return null;
 	}
 
-	/*
-	 * Updates user information
+	/**
+	 * Sends actual email with confirmation code
 	 *
-	 * returns true if successful and false if unsuccessful
+	 * @todo Implement this method
+	 */
+	public function sendConfirmationEmail() {
+		// TODO: Implement email sending
+	}
+
+	/**
+	 * Edits user information
 	 *
-	 * throws InputValidationException if there are problems with input data
+	 * @throws InputValidationException
+	 *
+	 * @todo ACtually implement checking if email is real
 	 */
 	public function processEditUser($user, $data)
 	{
 		$errors = array();
-
-		$has_username = !is_null($user->getUsername());
-
-		// don't change password if username was already set and no password fields are edited
-		$changepass = false;
-
-		// Force password setup when user sets username for the first time
-		if (!$has_username)
-		{
-			$changepass = true;
-		}
-		else if (array_key_exists('currentpass', $data) &&
-			array_key_exists('pass', $data) &&
-			array_key_exists('repeatpass', $data) &&
-			($data['currentpass'] != '' || $data['pass'] != '' || $data['repeatpass'] != ''))
-		{
-			$changepass = true;
-
-			if (!$user->checkPass($data['currentpass']))
-			{
-				$errors['currentpass'][] = 'You entered wrong current password';
-			}
-		}
-
-		if ($changepass)
-		{
-			// both passwords must be passed and non-empty
-			if (array_key_exists('pass', $data) && array_key_exists('repeatpass', $data) &&
-					($data['pass'] != '' || $data['repeatpass'] != '')
-				)
-			{
-				if (strlen($data['pass']) < 6)
-				{
-					$errors['pass'][] = 'Passwords must be at least 6 characters long';
-				}
-
-				if ($data['pass'] !== $data['repeatpass'])
-				{
-					$errors['repeatpass'][] = 'Passwords don\'t match';
-				}
-			}
-			else
-			{
-				if ($has_username)
-				{
-					$errors['pass'][] = 'You must specify new password';
-				}
-				else
-				{
-					$errors['pass'][] = 'You must set password when setting username and email';
-				}
-			}
-		}
-
-		// only validate username if user didn't specify it yet
-		if (!$has_username)
-		{
-			if (array_key_exists('username', $data))
-			{
-				$username = strtolower(trim(mb_convert_encoding($data['username'], 'UTF-8')));
-
-				if (strlen($username) < 2)
-				{
-					$errors['username'][] = 'Username must be at least 2 characters long';
-				}
-
-				if (strlen($username) > 25)
-				{
-					$errors['username'][] = 'Username must be no more then 25 characters long';
-				}
-
-				if (preg_match('/^[a-z][a-z0-9.]*[a-z0-9]$/', $username) !== 1)
-				{
-					$errors['username'][] = "Username must start with the letter and contain only latin letters, digits or '.' symbols";
-				}
-			}
-			else
-			{
-				$errors['username'][] = "No username passed";
-			}
-		}
-
-		if (array_key_exists('name', $data))
-		{
-			$name = trim(mb_convert_encoding($data['name'], 'UTF-8'));
-			if ($name == '')
-			{
-				$errors['name'][] = "Name can't be empty";
-			}
-		}
-		else
-		{
-			$errors['name'][] = 'No name specified';
-		}
 
 		if (array_key_exists('email', $data))
 		{
@@ -352,16 +319,6 @@ class EmailAuthenticationModule extends AuthenticationModule
 			$errors['email'][] = 'No email specified';
 		}
 
-		if (!$has_username)
-		{
-			$existing_users = User::getUsersByEmailOrUsername($username);
-			if (!array_key_exists('username', $errors) &&
-				(count($existing_users) > 0 && !$existing_users[0]->isTheSameAs($user))
-			) {
-				$errors['username'][] = "This username is already used, please pick another one";
-			}
-		}
-
 		$existing_users = User::getUsersByEmailOrUsername($email);
 		if (!array_key_exists('email', $errors) &&
 			(count($existing_users) > 0 && !$existing_users[0]->isTheSameAs($user))
@@ -374,95 +331,47 @@ class EmailAuthenticationModule extends AuthenticationModule
 			throw new InputValidationException('Validation failed', 0, $errors);
 		}
 
-		if ($changepass)
-		{
-			$user->setPass($data['pass']);
-			if ($has_username) {
-				$user->recordActivity(USERBASE_ACTIVITY_UPDATEPASS);
-			}
-		}
-
-		if (!$has_username)
-		{
-			$user->setUsername($username);
-			$user->recordActivity(USERBASE_ACTIVITY_ADDED_UPASS);
-		}
-
-		$user->setName($name);
 		$user->setEmail($email);
+//		$user->sendConfirmationEmail();
 		$user->save();
 
 		$user->recordActivity(USERBASE_ACTIVITY_UPDATEUSERINFO);
 
 		return true;
 	}
-
-	/*
-	 * Updates user's password
-	 *
-	 * returns true if successful and false if unsuccessful
-	 *
-	 * throws InputValidationException if there are problems with input data
-	 */
-	public function processUpdatePassword($user, $data)
-	{
-		$errors = array();
-
-		if (array_key_exists('pass', $data) ||
-			array_key_exists('repeatpass', $data))
-		{
-			if (array_key_exists('pass', $data) && array_key_exists('repeatpass', $data) && $data['pass'] !== $data['repeatpass'])
-			{
-				$errors['repeatpass'] = 'Passwords don\'t match';
-			}
-
-			if (array_key_exists('pass', $data) && strlen($data['pass']) < 6)
-			{
-				$errors['pass'] = 'Passwords must be at least 6 characters long';
-			}
-		}
-		else
-		{
-			$errors['pass'] = 'Passwords must be specified';
-		}
-
-		if (count($errors) > 0)
-		{
-			throw new InputValidationException('Validation failed', 0, $errors);
-		}
-
-		$user->setPass($data['pass']);
-		$user->setRequiresPasswordReset(false);
-		$user->save();
-
-		$user->resetTemporaryPassword();
-
-		$user->recordActivity(USERBASE_ACTIVITY_RESETPASS);
-
-		return true;
-	}
-
-	// THIS SHOULD ONLY BE SET ON PASSWORD RESET PAGE
-	// SETTING THIS ON OTHER PAGES CAN RESULT IN SECURITY BREACH
-	public static $IGNORE_PASSWORD_RESET = false;
 }
 
 /**
+ * Email credentials for the user
+ *
  * @package StartupAPI
  * @subpackage Authentication\Email
  */
 class EmailUserCredentials extends UserCredentials {
-	private $username;
+	/**
+	 * @var string User's email address
+	 */
+	private $email;
 
-	public function __construct($username) {
-		$this->username = $username;
+	/**
+	 * Creates user credentials object
+	 *
+	 * @param string $email User's email address
+	 */
+	public function __construct($email) {
+		$this->email = $email;
 	}
 
-	public function getUsername() {
-		return $this->username;
+	/**
+	 * Returns user's email address
+	 *
+	 * @return string User's email address
+	 */
+	public function getEmail() {
+		return $this->email;
 	}
 
 	public function getHTML() {
-		return $this->username;
+		return $this->email;
 	}
 }
