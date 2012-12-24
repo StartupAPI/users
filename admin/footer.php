e289ad605be363736d61a22e52fe13834d1287b0
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2012-12-24 01:50:16 -0500

    Fixed license file link

--- admin/footer.php
+++ admin/footer.php
@@ -4,7 +4,7 @@
 		<footer class="footer navbar-inner">
 			<div class="container">
 				<div class="pull-right"><a href="http://www.startupapi.com/" target="_blank"><img src="<?php echo UserConfig::$USERSROOTURL ?>/images/powered_by_logo.png" width="149" height="67"/></a></div>
-				<p>Powered by <a href="http://www.startupapi.com/" target="_blank">Startup API v<?php echo StartupAPI::getVersion() ?></a>. Code licensed under the <a href="https://github.com/sergeychernyshev/users/blob/master/LICENSE" target="_blank">MIT License</a>.</p>
+				<p>Powered by <a href="http://www.startupapi.com/" target="_blank">Startup API v<?php echo StartupAPI::getVersion() ?></a>. Code licensed under the <a href="https://github.com/StartupAPI/users/blob/master/LICENSE-MIT" target="_blank">MIT License</a>.</p>
 				<p>This application uses <a href="http://twitter.github.com/bootstrap/" target="_blank">Twitter Bootstrap</a>.</p>
 				<p>Icons from <a href="http://glyphicons.com" target="_blank">Glyphicons Free</a>, licensed under <a href="http://creativecommons.org/licenses/by/3.0/" target="_blank">CC BY 3.0</a>.</p>
 
