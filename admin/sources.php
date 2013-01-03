2f72d97074b52a562abf70d2034d42edcfa1c2c7
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2013-01-03 08:40:00 -0500

    Formatted the sources page

--- admin/sources.php
+++ admin/sources.php
@@ -8,15 +8,15 @@ $days = 30;
 
 $sources = User::getReferers($days);
 uasort($sources, function($a, $b) {
-  return count($b) - count($a);
-  });
+			return count($b) - count($a);
+		});
 ?>
 <div class="span9">
 	<p>Sources that attracted the most registered users in the <b>last <?php echo $days ?> days</b>.</p>
 	<table class="table">
 		<?php foreach ($sources as $source => $users) { ?>
 			<tr>
-				<td><a href="<?php echo UserTools::escape($source) ?>" target="_blank"><?php echo UserTools::escape(substr($source,0,40)) ?><?php if (strlen($source) > 40) { ?>...<?php } ?></a></td>
+				<td><a href="<?php echo UserTools::escape($source) ?>" target="_blank"><?php echo UserTools::escape(substr($source, 0, 40)) ?><?php if (strlen($source) > 40) { ?>...<?php } ?></a></td>
 				<td><span class="badge"><?php echo count($users) ?> users</span></td>
 				<td>
 					<?php foreach ($users as $user) { ?>
