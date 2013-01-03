e7a9f0479563750638a8ca973e5f181c1af75075
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2013-01-02 21:38:35 -0500

    Fixed display when no campaign data gathered

--- admin/campaigns.php
+++ admin/campaigns.php
@@ -22,37 +22,51 @@ $tables = array(
 	'cmp_keywords' => 'Campaign Keywords',
 	'cmp_content' => 'Campaign content'
 );
-?>
-<div class="span9">
-	Campaigns that attracted the most registered users in the <b>last <?php echo $days ?> days</b>
-</div>
-<?php
-foreach ($tables as $slug => $header) {
+if (count($campaigns) > 0) {
 	?>
 	<div class="span9">
-		<h3><?php echo $header ?></h3>
-		<table class="table">
-			<?php
-			$sources = $campaigns[$slug];
-			uasort($sources, function($a, $b) {
-						return count($b) - count($a);
-					});
+		Campaigns that attracted the most registered users in the <b>last <?php echo $days ?> days</b>
+	</div>
+	<?php
+	foreach ($tables as $slug => $header) {
+		?>
+		<div class="span9">
+			<h3><?php echo $header ?></h3>
+			<table class="table">
+				<?php
+				$sources = $campaigns[$slug];
+				uasort($sources, function($a, $b) {
+							return count($b) - count($a);
+						});
 
-			foreach ($sources as $source => $users) {
+				foreach ($sources as $source => $users) {
+					?>
+					<tr>
+						<td><b title="<?php echo UserTools::escape($source) ?>"><?php echo UserTools::escape(substr($source, 0, 40)) ?><?php if (strlen($source) > 40) { ?>...<?php } ?></b></td>
+						<td><span class="badge"><?php echo count($users) ?> users</span></td>
+						<td>
+							<?php
+							foreach ($users as $user) {
+								?>
+								<a style="margin-right: 0.5em" href="<?php echo UserConfig::$USERSROOTURL ?>/admin/user.php?id=<?php echo $user->getID(); ?>">
+									<i class="icon-user"></i> <?php echo UserTools::escape($user->getName()) ?>
+								</a>
+								<?php
+							}
+							?>
+						</td>
+					</tr>
+					<?php
+				}
 				?>
-				<tr>
-					<td><b title="<?php echo UserTools::escape($source) ?>"><?php echo UserTools::escape(substr($source, 0, 40)) ?><?php if (strlen($source) > 40) { ?>...<?php } ?></b></td>
-					<td><span class="badge"><?php echo count($users) ?> users</span></td>
-					<td>
-		<?php foreach ($users as $user) { ?>
-							<a style="margin-right: 0.5em" href="<?php echo UserConfig::$USERSROOTURL ?>/admin/user.php?id=<?php echo $user->getID(); ?>">
-								<i class="icon-user"></i> <?php echo UserTools::escape($user->getName()) ?>
-							</a>
-				<?php } ?>
-					</td>
-				</tr>
-	<?php } ?>
-		</table>
+			</table>
+		</div>
+		<?php
+	}
+} else {
+	?>
+	<div class="span9">
+		No sign-ups from campaigns within the <b>last <?php echo $days ?> days</b>
 	</div>
 	<?php
 }
