1957a4c6d044a5f0b62b05d82410ca42457782eb
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2013-09-25 19:09:36 -0400

    Updated Bootstrap and jQuery

--- classes/StartupAPI.php
+++ classes/StartupAPI.php
@@ -73,7 +73,7 @@ class StartupAPI {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="<?php echo $bootstrapCSS ?>" rel="stylesheet">
 		<link href="<?php echo UserConfig::$USERSROOTURL ?>/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
-		<script src="<?php echo UserConfig::$USERSROOTURL ?>/jquery-1.8.2.min.js"></script>
+		<script src="<?php echo UserConfig::$USERSROOTURL ?>/jquery-1.10.2.min.js"></script>
 		<script src="<?php echo UserConfig::$USERSROOTURL ?>/bootstrap/js/bootstrap.min.js"></script>
 
 		<link rel="stylesheet" type="text/css" href="<?php echo UserConfig::$USERSROOTURL ?>/themes/<?php echo UserConfig::$theme ?>/startupapi.css">
