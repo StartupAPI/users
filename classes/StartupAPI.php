d557220eeabcf4f15ccfd9c2e03013f60075ccc0
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2013-12-21 02:55:15 +0000

    Added file and line number to DB Exceptions

--- classes/StartupAPI.php
+++ classes/StartupAPI.php
@@ -366,22 +366,24 @@ class DBException extends StartupAPIException {
 		$exception_message = $message;
 
 		$class = get_class($this);
+		$file = self::getFile();
+		$line = self::getLine();
 
 		if (is_null($db)) {
-			$exception_message = "[$class] Can't connect to database, \$db object is null";
+			$exception_message = "[$class] Can't connect to database, \$db object is null (in $file on line $line)";
 		} else if ($db->connect_error) {
 			$exception_message = "[$class] Can't connect to database: (" . $db->connect_errno . ") " .
-					$db->connect_error;
+					$db->connect_error . " (in $file on line $line)";
 		} else if ($db->error) {
-			$exception_message = "[$class] DB Error: " . $db->error;
+			$exception_message = "[$class] DB Error: " . $db->error . " (in $file on line $line)" ;
 		} else if (!$stmt) {
 			$exception_message = "[$class]" .
 					' $db->error: ' . $db->error .
-					' with message: ' . $message;
+					' with message: ' . $message . " (in $file on line $line)";
 		} else {
 			$exception_message = "[$class]" .
 					' $stmt->error: ' . $stmt->error .
-					' with message: ' . $message;
+					' with message: ' . $message . " (in $file on line $line)";
 		}
 
 		parent::__construct($exception_message, $code, $previous);
