ac6dd8bb9ee341e0e2c9bdca707e18264ccbdc2d
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2013-12-23 20:27:26 -0500

    Merged all admin views into pages themselves

--- admin/outstanding.php
+++ admin/outstanding.php
@@ -1,9 +1,35 @@
 <?php
 require_once(__DIR__.'/admin.php');
 
-$ADMIN_SECTION = 'outstanding';
+/* ------------------- Preparing data for template ------------------------------------- */
+# Getting users one by one is quite ineffective, so building query to do this
+$db = UserConfig::getDB();
 
-include(__DIR__.'/view/outstanding.php');
+if (!($stmt = $db->prepare('SELECT a.id, a.name, a.plan_slug, a.schedule_slug, SUM(c.amount) AS debt FROM ' . UserConfig::$mysql_prefix .
+		'accounts AS a JOIN ' . UserConfig::$mysql_prefix . 'account_charge AS c ON a.id = c.account_id GROUP BY c.account_id HAVING debt < 0'))) {
+	throw new DBPrepareStmtException($db);
+}
+
+if (!$stmt->execute()) {
+	throw new DBExecuteStmtException($db, $stmt);
+}
+
+if (!$stmt->bind_result($id, $name, $plan, $schedule, $debt)) {
+	throw new DBBindResultException($db, $stmt);
+}
+
+$debtors = array();
+while ($stmt->fetch() === TRUE) {
+	$debtors[] = array('id' => $id, 'name' => $name, 'plan' => $plan, 'schedule' => $schedule, 'debt' => sprintf("%.2f", abs($debt)));
+}
+
+$stmt->close();
+
+$template_data['debtors'] = $debtors;
+$template_data['USERSROOTURL'] = UserConfig::$USERSROOTURL;
+/* ------------------- / Preparing data for template -------------------------------------- */
+
+$ADMIN_SECTION = 'outstanding';
 
 require_once(__DIR__.'/header.php');
 
