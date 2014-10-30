cc5f8573b287430346d7228115ffa96626c2de91
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2014-10-30 16:21:53 -0400

    Migrated email verification to Twig, added wrappers pages that inherited from base page.

--- send_email_verification_code.php
+++ send_email_verification_code.php
@@ -13,31 +13,7 @@ if (!is_null($email)) {
 	$user->sendEmailVerificationCode();
 }
 
-require_once(UserConfig::$header);
-?>
-<div class="container-fluid" style="margin-top: 1em">
-	<div class="row-fluid">
-		<div class="span12">
-			<?php
-			if (is_null($email)) {
-				?>
-				<div id="startupapi-sendverificationcode-failure">
-					<h3>No email address provided</h3>
-					<p>You don't have an email set for your profile.</p>
-					<a class="btn btn-primary" href="<?php echo UserConfig::$USERSROOTURL ?>/edit.php">Click here to add email address.</a>
-				</div>
-			<?php } else {
-				?>
-				<div id="startupapi-sendverificationcode-success">
-					<h3>Verification code sent</h3>
-					<p>Verification code was sent to <span class="startupapi-email-to-verify"><?php echo UserTools::escape($user->getEmail()) ?></span>.</p>
-					<a class="btn" href="<?php echo UserConfig::$USERSROOTURL ?>/verify_email.php">Click here when you receive it</a>
-				</div>
-				<?php
-			}
-			?>
-		</div>
-	</div>
-</div>
-<?php
-require_once(UserConfig::$footer);
+$template_info = StartupAPI::getTemplateInfo();
+$template_info['email'] = $email;
+
+StartupAPI::$template->display('send_email_verification_code.html.twig', $template_info);
--- verify_email.php
+++ verify_email.php
@@ -8,17 +8,17 @@ UserConfig::$IGNORE_REQUIRED_EMAIL_VERIFICATION = true;
 $user = User::get();
 $email = is_null($user) ? null : $user->getEmail();
 
-$errors = array();
+$template_info = StartupAPI::getTemplateInfo();
 
-$verification_complete = false;
+$errors = array();
 
 if (array_key_exists('code', $_GET)) {
 	try {
 		if (User::verifyEmailLinkCode($_GET['code'], $user)) {
-			$verification_complete = true;
+			$template_info['verification_complete'] = true;
 		} else {
 			throw new InputValidationException('Invalid code', 0, array(
-				'code' => array('Invalid code')
+				'code' => 'Invalid code'
 			));
 		}
 	} catch (InputValidationException $ex) {
@@ -26,78 +26,13 @@ if (array_key_exists('code', $_GET)) {
 	}
 }
 
-require_once(UserConfig::$header);
-?>
-<div class="container-fluid" style="margin-top: 1em">
-	<div class="row-fluid">
-		<div class="span12">
-			<?php
-			if ($verification_complete) {
-				$return = User::getReturn();
-				User::clearReturn();
-				if (is_null($return)) {
-					$return = UserConfig::$DEFAULT_EMAIL_VERIFIED_RETURN;
-				}
-				?>
-				<div id="startupapi-verifyemail-success">
-					<h3>Thank you for verifying your email address</h3>
-					<p>You successfully verified your email address.</p>
-					<a class="btn btn-primary" href="<?php echo UserTools::escape($return) ?>">Click here to continue.</a>
-				</div>
-				<?php
-			} else {
-				?>
-				<div>
-					<?php
-					if (count($errors) > 0) {
-						?>
-						<div class="alert alert-block alert-error fade-in">
-							<h4 style="margin-bottom: 0.5em">Snap!</h4>
-							<ul>
-								<?php
-								foreach ($errors as $field => $errorset) {
-									foreach ($errorset as $error) {
-										?>
-										<li>
-											<label  style="cursor: pointer" for="startupapi-code">
-												<?php echo $error ?>
-											</label>
-										</li>
-										<?php
-									}
-									?>
-								</ul>
-							</div>
-							<?php
-						}
-					}
-					?>
-					<h3>Please verify your email address</h3>
-					<p>Confirmation code was sent to <?php echo!is_null($email) ? '<span class="startupapi-email-to-verify">' . UserTools::escape($email) . '</span>' : 'your email address' ?><br/>Please enter it below and click verify button.</p>
+$template_info['email'] = $email;
+$template_info['errors']['verify_email'] = $errors;
 
-					<form class="form well" action="" method="GET">
-						<fieldset>
-							<div class="control-group<?php if (array_key_exists('code', $errors)) { ?> error" title="<?php echo UserTools::escape(implode("\n", $errors['code'])) ?><?php } ?>">
-								<label class="control-label" for="startupapi-code">Confirmation Code</label>
-								<div class="controls">
-									<input id="startupapi-code" name="code" type="text" autocomplete="off"/>
-									<a id="startupapi-verifyemail-resend" href="<?php echo UserConfig::$USERSROOTURL ?>/send_email_verification_code.php">I never got the code, please resend</a>
-								</div>
-							</div>
-							<div class="control-group">
-								<div class="controls">
-									<button class="btn btn-primary" type="submit">Verify</button>
-								</div>
-							</div>
-						</fieldset>
-					</form>
+$template_info['return'] = User::getReturn();
+User::clearReturn();
+if (is_null($template_info['return'])) {
+	$template_info['return'] = UserConfig::$DEFAULT_EMAIL_VERIFIED_RETURN;
+}
 
-				</div>
-				<?php
-			}
-			?>
-		</div>
-	</div>
-</div>
-<?php
-require_once(UserConfig::$footer);
+StartupAPI::$template->display('verify_email.html.twig', $template_info);
