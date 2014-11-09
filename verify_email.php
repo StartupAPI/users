3d3c9c9ce940a3850f506071970c4f5559ca2ea3
Sergey Chernyshev <github.com@antispam.sergeychernyshev.com>
2014-11-09 00:32:29 -0500

    Simplified error logic

--- verify_email.php
+++ verify_email.php
@@ -10,27 +10,19 @@ $email = is_null($user) ? null : $user->getEmail();
 
 $template_info = StartupAPI::getTemplateInfo();
 
-$errors = array();
-
 if (array_key_exists('code', $_GET)) {
-	try {
-		if (User::verifyEmailLinkCode($_GET['code'], $user)) {
-			$template_info['verification_complete'] = true;
-		} else {
-			throw new InputValidationException('Invalid code', 0, array(
-				'code' => 'Invalid code'
-			));
-		}
-	} catch (InputValidationException $ex) {
-		$errors = $ex->getErrors();
+	if (User::verifyEmailLinkCode($_GET['code'], $user)) {
+		$template_info['verification_complete'] = true;
+	} else {
+		$template_info['errors']['verify_email'][] = array('code' => 'Invalid code');
 	}
 }
 
 $template_info['email'] = $email;
-$template_info['errors']['verify_email'] = $errors;
 
 $template_info['return'] = User::getReturn();
 User::clearReturn();
+
 if (is_null($template_info['return'])) {
 	$template_info['return'] = UserConfig::$DEFAULT_EMAIL_VERIFIED_RETURN;
 }
