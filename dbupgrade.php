834215aa0bc783abfc60a139b728e1813bc50d1d
Alex <alex.druk@gmail.com>
2016-08-10 19:01:05 +0200

    fixed downgrade issie 236

--- dbupgrade.php
+++ dbupgrade.php
@@ -21,19 +21,19 @@ $versions[_]['down'][]	= "";
 
 /* -------------------------------------------------------------------------------------------------------
  * VERSION 36
- * corrected type 
+ * corrected type
 */
-$versions[36]['up'][] = "ALTER TABLE `u_user_badges` MODIFY COLUMN `time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+$versions[36]['up'][] = "ALTER TABLE `u_user_badges` MODIFY COLUMN `time` DATETIME NULL
 	COMMENT 'Time when user got the badge'";
-$versions[36]['up'][] = "ALTER TABLE `u_users` MODIFY COLUMN `email_verification_code_time` DATETIME DEFAULT CURRENT_TIMESTAMP 
+$versions[36]['up'][] = "ALTER TABLE `u_users` MODIFY COLUMN `email_verification_code_time` DATETIME NULL DEFAULT NULL
 	COMMENT 'Email verification code generation time'";
-$versions[36]['up'][] = "ALTER TABLE `u_users` MODIFY COLUMN `last_accessed` DATETIME  DEFAULT CURRENT_TIMESTAMP";
+$versions[36]['up'][]	= 'ALTER TABLE u_users DROP COLUMN last_accessed';
 
 $versions[36]['down'][] = "ALTER TABLE `u_user_badges` MODIFY COLUMN `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
   COMMENT 'Time when user got the badge'";
-$versions[36]['down'][] = "ALTER TABLE `u_users` MODIFY COLUMN `email_verification_code_time` timestamp NULL DEFAULT NULL  
+$versions[36]['down'][] = "ALTER TABLE `u_users` MODIFY COLUMN `email_verification_code_time` TIMESTAMP NULL  DEFAULT NULL
 	COMMENT 'Email verification code generation time'";
-$versions[36]['down'][] = "ALTER TABLE `u_users` MODIFY COLUMN `last_accessed` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP";
+$versions[36]['down'][]	= 'ALTER TABLE u_users ADD last_accessed DATETIME';
 
 /* -------------------------------------------------------------------------------------------------------
  * VERSION 35
