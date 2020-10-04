4ded1a3a5928fbc946504ad01caf06176921a260
Sergey Chernyshev <sergey.chernyshev@gmail.com>
2020-10-04 00:36:08 -0400

    Removed deprecated substring chracted curly brace syntax

--- classes/Invitation.php
+++ classes/Invitation.php
@@ -455,15 +455,15 @@ class Invitation {
 		$chars_length = (strlen($chars) - 1);
 
 		// Start our string
-		$string = $chars{rand(0, $chars_length)};
+		$string = $chars[rand(0, $chars_length)];
 
 		// Generate random string
 		for ($i = 1; $i < $length; $i = strlen($string)) {
 			// Grab a random character from our list
-			$r = $chars{rand(0, $chars_length)};
+			$r = $chars[rand(0, $chars_length)];
 
 			// Make sure the same two characters don't appear next to each other
-			if ($r != $string{$i - 1})
+			if ($r != $string[$i - 1])
 				$string .= $r;
 		}
 
